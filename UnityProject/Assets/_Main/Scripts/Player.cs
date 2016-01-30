c6bef0d86cbf6aef6cd6d12a8566b56e9a70ba7c
olivie_j <j.olivier@student.isartdigital.com>
2016-01-30 14:31:43 +0100

    add move player

--- UnityProject/Assets/_Main/Scripts/Player.cs
+++ UnityProject/Assets/_Main/Scripts/Player.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using XboxCtrlrInput;
 
-public class Player : MonoBehaviour {
+[RequireComponent (typeof (Rigidbody))]
 
+public class Player : MonoBehaviour
+{
 	public Color color;
 	public float speed;
+
+	public XboxController controller;
+
+	private Rigidbody rigidbody;
 
 	public List<Zone> OwnedZones { get; private set; }
 	public List<ZoneEffect> ActiveEffects { get; private set; }
 	public Zone Position { get; private set; }
+
+	private void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody> ();
+	}
 
 	public Player() : base() 
 	{
@@ -32,16 +44,27 @@ public class Player : MonoBehaviour {
 		Position = null;
 	}
 
-	#warning TODO : ReadInput.
 	private PlayerInput ReadInput() 
 	{
-		return default(PlayerInput);
+		PlayerInput input;
+
+		input.moveAxis.x = XCI.GetAxis(XboxAxis.LeftStickX, controller);
+		input.moveAxis.y = XCI.GetAxis(XboxAxis.LeftStickY, controller);
+		input.validateZoneButtonPressed = XCI.GetButtonDown (XboxButton.LeftBumper, controller);
+
+		if(XCI.GetAxis(XboxAxis.LeftTrigger, controller) != 0)
+			input.fireButtonPressed = true;
+		else
+			input.fireButtonPressed = false;
+		return input;
 	}
 
 	#warning TODO : UpdatePlayer
 	private void UpdatePlayer(PlayerInput input) 
 	{
-		
+		Vector3 forceRigid = new Vector3 (input.moveAxis.x, 0, input.moveAxis.y);
+
+		rigidbody.AddForce (forceRigid * speed, ForceMode.VelocityChange);
 	}
 
 	public void GameUpdate()
