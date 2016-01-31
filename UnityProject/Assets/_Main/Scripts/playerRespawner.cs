e8382df341bb60471404f00159d4166e29eeadb2
olivie_j <j.olivier@student.isartdigital.com>
2016-01-31 14:49:50 +0100

    up

---
+++ UnityProject/Assets/_Main/Scripts/playerRespawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class playerRespawner : MonoBehaviour {
+
+	void OnTriggerEnter(Collider c)
+	{
+		Player p = c.GetComponent<Player> ();
+		if (p)
+		{
+			if (Game.Instance.Zones == null || Game.Instance.Zones.Count == 0)
+				return;
+
+			List<Zone> possibleZones = new List<Zone> ();
+			foreach (Zone z in Game.Instance.Zones)
+				if (z.VisitorsCount == 0)
+					possibleZones.Add (z);
+
+			if (possibleZones.Count == 0)
+				p.transform.position = Game.Instance.Zones [0].transform.position; // cas particulier
+			else
+				p.transform.position = possibleZones [Random.Range (0, possibleZones.Count)].transform.position;
+		}
+	}
+}
