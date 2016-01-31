368d01b011ffa37e95ac7d0cfcfb22f77f04f5e2
olivie_j <j.olivier@student.isartdigital.com>
2016-01-31 02:22:28 +0100

    up

--- UnityProject/Assets/_Main/Scripts/Player.cs
+++ UnityProject/Assets/_Main/Scripts/Player.cs
@@ -115,6 +115,7 @@ public class Player : MonoBehaviour
 		{
 			if (Position && Position.CanBeTakenOver && Position.Owner != this)
 			{
+				//SON Prise controlle zone
 				if (Position.Owner != null)
 					Position.Owner.OnZoneLost (Position);
 				this.OnZoneWon (Position);
@@ -123,6 +124,7 @@ public class Player : MonoBehaviour
 
 		if (input.fireButtonPressed && rightBullet)
 		{
+			//SON Tire
 			bulletReference = Instantiate(Bullet, transform.position, transform.rotation) as GameObject;
 			bulletReference.GetComponent<MoveBullet> ().playerOwner = this.gameObject;
 			bulletReference.GetComponent<MoveBullet> ().multiSpeedBullet = this.multiplicatorSpeedBullet;
