9f418a761eb1843056583b09366cd366a798ba10
Sylafrs <sylaf@hotmail.fr>
2016-01-31 06:06:57 +0100

    purge de scene

--- UnityProject/Assets/_Main/Scripts/Menu.cs
+++ UnityProject/Assets/_Main/Scripts/Menu.cs
@@ -2,9 +2,18 @@
 using System.Collections;
 using XboxCtrlrInput;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class Menu : MonoBehaviour
 {
+	public Image[] Join;
+	public Image[] Ready;
+
+	public UnityEvent OnGameStart;
+
+	bool WaitingPlayers;
+
 	Dictionary<XboxController, Player> Players = new Dictionary<XboxController, Player>();
 
 	void Start()
@@ -13,23 +22,64 @@ public class Menu : MonoBehaviour
 			Players.Add ((XboxController)i, null);
 	}
 
-	bool gameStarted;
-	void Update()
+	public void Quit()
+	{
+		Application.Quit();
+	}
+
+	public void StartWaitingForPlayers()
 	{
 		for (int i = 1; i < 5; i++)
 		{
-			if (Players [(XboxController)i] == null) {
-				if (XCI.GetButtonDown (XboxButton.A, (XboxController)i)) {
-					Players [(XboxController)i] = Player.Create((XboxController)i);
+			if (Players [(XboxController)i] != null)
+			{
+				GameObject.Destroy (Players [(XboxController)i].gameObject);
+				Players [(XboxController)i] = null;
+				Join[i - 1].enabled = false;
+				Ready[i - 1].enabled = true;	
+			}
+		}
+
+		WaitingPlayers = true;
+	}
+
+	public void StopWaiting()
+	{
+		WaitingPlayers = false;
+	}
+
+	bool gameStarted;
+	void Update()
+	{
+		if (!WaitingPlayers)
+			return;
+				
+		for (int i = 1; i < 5; i++)
+		{
+			if (Players [(XboxController)i] == null) 
+			{
+				if (XCI.GetButtonDown (XboxButton.A, (XboxController)i))
+				{
+					Player p = Player.Create((XboxController)i);
+					p.gameObject.SetActive(false);
+					Players[(XboxController)i] = p;
+
+					Join[i - 1].enabled = true;
+					Ready[i - 1].enabled = false;
 				}
-			} else {
+			} 
+			else 
+			{
 				if (XCI.GetButtonDown (XboxButton.B, (XboxController)i))
 				{
 					GameObject.Destroy (Players [(XboxController)i].gameObject);
 					Players [(XboxController)i] = null;
+					Join[i - 1].enabled = false;
+					Ready[i - 1].enabled = true;
 				}
 
-				if (XCI.GetButtonDown (XboxButton.Start, (XboxController)i)) {
+				if (XCI.GetButtonDown (XboxButton.Start, (XboxController)i))
+				{
 					if (!gameStarted)
 					{
 						gameStarted = true;
@@ -46,9 +96,12 @@ public class Menu : MonoBehaviour
 		List<Player> players = new List<Player> ();
 		foreach (Player p in Players.Values)
 			if (p != null) {
+				p.gameObject.SetActive(true);
 				players.Add (p);
 				GameObject.DontDestroyOnLoad (p.gameObject);
 			}
 		Game.Instance.StartGame (players);
+		OnGameStart.Invoke();
+		StopWaiting();
 	}
 }
