c21a1b22f4f7c02ff6f637fd077f3e959c595f04
Sylafrs <sylaf@hotmail.fr>
2016-01-31 05:10:59 +0100

    add some effects

---
+++ UnityProject/Assets/_Main/Scripts/ZoneEffects/ZoneEffectFlipCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu]
+public class ZoneEffectFlipCamera : ZoneEffect 
+{
+	public override IEnumerator ApplyEffect (Player player)
+	{
+		CameraFlip flipper_le_dauphin = Camera.main.GetComponent<CameraFlip>();
+		Zone z = player.LastOwnedZone;
+
+		flipper_le_dauphin.inverse = true;
+		yield return player.WaitUntilLastZoneModified();
+		flipper_le_dauphin.inverse = (z != player.LastOwnedZone);
+	}
+
+	public override void ApplyInputBuff (ref PlayerInput data)
+	{
+		// Does nothing
+	}
+}
---
+++ UnityProject/Assets/_Main/Scripts/ZoneEffects/ZoneEffectShootStop.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu]
+public class ZoneEffectShootStop : ZoneEffect {
+
+	public override IEnumerator ApplyEffect (Player player)
+	{
+		foreach(Player p in Game.Instance.Players)
+			if(p != player)
+				player.AddBuff (this);
+
+		yield return player.WaitUntilLastZoneModified();
+
+		foreach(Player p in Game.Instance.Players)
+			if(p != player)
+				player.RemoveBuff (this);
+	}
+
+	public override void ApplyInputBuff (ref PlayerInput data)
+	{
+		data.fireButtonPressed = false;
+	}
+}
